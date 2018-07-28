8c0f6d16030318898fc9e33783973aecfbc0723e
DenilsonDesigns <daniellanguilller@gmail.com>
2018-07-28 22:37:59 +1000

    added mongo data persistence

--- app.js
+++ app.js
@@ -1,43 +1,84 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const mongoose = require('mongoose');
 
+mongoose.connect('mongodb://localhost/yelp_camp');
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
-const campgrounds = [
-   {name: 'Salmon Creek', image: 'https://usatunofficial.files.wordpress.com/2015/08/crane-flat-site-503.jpg'},
-   {name: 'Granite Hill', image: 'https://travel.home.sndimg.com/content/dam/images/travel/fullrights/2016/01/14/national-park-camping/camping-glacier-national-park-camping.jpg.rend.hgtvcom.966.725.suffix/1491593018562.jpeg'},
-   {name: 'Mountain Goat Rest', image: 'https://www.straight.com/files/v3/styles/gs_large/public/images/18/06/gettyimages-649155058.jpg?itok=Lhx5ciAR'},
-   {name: 'Mountain Goat Rest', image: 'https://www.straight.com/files/v3/styles/gs_large/public/images/18/06/gettyimages-649155058.jpg?itok=Lhx5ciAR'},
-   {name: 'Mountain Goat Rest', image: 'https://www.straight.com/files/v3/styles/gs_large/public/images/18/06/gettyimages-649155058.jpg?itok=Lhx5ciAR'},
-   {name: 'Mountain Goat Rest', image: 'https://www.straight.com/files/v3/styles/gs_large/public/images/18/06/gettyimages-649155058.jpg?itok=Lhx5ciAR'},
-   {name: 'Mountain Goat Rest', image: 'https://www.straight.com/files/v3/styles/gs_large/public/images/18/06/gettyimages-649155058.jpg?itok=Lhx5ciAR'},
-];
+//SCHEMA SETUP
+const campgroundSchema = new mongoose.Schema({
+   name: String,
+   image: String,
+   description: String
+});
+
+const Campground = mongoose.model('Campgrounds', campgroundSchema);
+
+// Campground.create({name: 'Granite Hill', image: 'https://travel.home.sndimg.com/content/dam/images/travel/fullrights/2016/01/14/national-park-camping/camping-glacier-national-park-camping.jpg.rend.hgtvcom.966.725.suffix/1491593018562.jpeg',
+// description: "This is a huge granite hill, no bathrooms"
+// }, (err, campground)=>{
+//    if (err) {
+//       console.log(err);
+//    } else {
+//       console.log('NEWLY CREATED CAMPGROUND');
+//       console.log(campground);
+//    }
+// });
+
 
 app.get('/', (req,res)=>{
    res.render('landing');
 });
 
 app.get('/campgrounds',(req,res)=>{
-   res.render('campgrounds',{campgrounds:campgrounds});
+   //GET ALL CAMPGROUNDS FROM DB
+   Campground.find({}, (err,allCampgrounds)=>{
+      if (err) {
+         console.log(err)
+      } else {
+         res.render('index',{campgrounds:allCampgrounds}); 
+      }
+   });
 });
 
+//CREATE- ADD NEW CAMPGROUND
 app.post('/campgrounds', (req,res)=>{
    //GET DATA FROM FORM AND ADD TO CAMPGROUNDS ARRAY
    const name = req.body.name;
    const image = req.body.image;
-   const newCampground = {name: name, image: image};
-   campgrounds.push(newCampground);
-   //REDIRECT BACK TO CAMPGROUNDS
-   res.redirect('/campgrounds');
+   const description = req.body.description;
+   const newCampground = {name: name, image: image, description: description};
+   //CREATE A NEW CAMPGROUND AND SAVE TO DB
+   Campground.create(newCampground, (err, newlyCreated)=>{
+      if (err) {
+         console.log(err);
+      } else {
+         //REDIRECT BACK TO CAMPGROUNDS
+         res.redirect('/campgrounds'); 
+      }
+   });
 });
 
 app.get('/campgrounds/new',(req,res)=>{
-   res.render('new.ejs');
+   res.render('new');
+});
+
+//SHOW- SHOWS MORE INFO ABOUT ONE CAMPGROUND
+app.get('/campgrounds/:id', (req,res)=>{
+   //FIND CAMPGROUND WITH PROVIDED ID
+   Campground.findById(req.params.id, (err, foundCampground)=>{
+      if (err) {
+         console.log(err);
+      } else {
+         //RENDER SHOW TEMPLATE WITH THAT CAMPGROUND
+         res.render('show', {campground: foundCampground});
+      }
+   });
 });
 
 const PORT = 3000;
 app.listen(PORT, ()=>{
-   console.log(`YelpCamp server has started: PORT: ${3000}`);
+   console.log(`YelpCamp server has started: PORT: ${PORT}`);
 });
